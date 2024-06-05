bb876fd85e44d37e60b5a81c3eeac86bf6621378
Anastasia Shurenkova <anastasia.k.shurenkova@yandex.ru>
2024-06-05 11:59:47 +0300

    First commit

---
+++ tests/test_bun.py
@@ -0,0 +1,26 @@
+class TestBun:
+    # def test_bun_return_name(self):
+    #     data_base = Database()
+    #     bun_choice = random.choice(data_base.available_buns())
+    #     bun_name = bun_choice.name
+    #     bun_get_name = bun_choice.get_name()
+    #
+    #     assert bun_get_name == bun_name
+
+    # def test_bun_return_price(self):
+    #     data_base = Database()
+    #     bun_choice = random.choice(data_base.available_buns())
+    #     bun_price = bun_choice.price
+    #     bun_get_price = bun_choice.get_price()
+    #
+    #     assert bun_get_price == bun_price
+    def test_bun_return_name(self, bun_choice):
+        bun_name = bun_choice.name
+        bun_get_name = bun_choice.get_name()
+
+        assert bun_get_name == bun_name
+    def test_bun_return_price(self, bun_choice):
+        bun_price = bun_choice.price
+        bun_get_price = bun_choice.get_price()
+
+        assert bun_get_price == bun_price
